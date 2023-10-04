77f5878ae6110f43bfe883ab63e5690d352a2de9
Gellén <gellenrebeka@gmail.com>
2023-10-04 11:09:26 +0200

    na tán így jó lesz a macsek??

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// változók 
+
 const directions = ["left", "right", "up"];
 
 const leftButton = document.getElementById("left");
@@ -5,11 +7,22 @@ const upButton = document.getElementById("up");
 const rightButton = document.getElementById("right");
 const catImage = document.getElementById("catImage");
 
+let playerScore = 0;
+let catScore = 0;
+
+// függvények
+// kezdő cicakép beállítása
+function setInitialImage() {
+    catImage.src = "assets/cat/front.jpg";
+}
+
+// random irány generálása, ez lesz a számítógép által választott irány
 function generateRandomDirection() {
   const randomIndex = Math.floor(Math.random() * directions.length);
   return directions[randomIndex];
 }
 
+// kép frissítése a megfelelő irány szerint
 function updateImage(direction) {
     switch(direction) {
         case "left":
@@ -26,19 +39,46 @@ function updateImage(direction) {
     }
 }
 
+// pontszám frissítése
+function updateScore(id) {
+    if (id === "player-score") {
+        playerScore++;
+        document.getElementById("player-score").innerHTML = playerScore;
+    } else {
+        catScore++;
+        document.getElementById("cat-score").innerHTML = catScore;
+    }
+   
+}
+
+function playRound(playerDirection, catDirection) {
+    if (playerDirection === catDirection) {
+        updateScore("player-score");
+    } else {
+        updateScore("cat-score");
+    }
+}
+
+function playGame() {
+    const playerDirection = button.id;
+    const catDirection = generateRandomDirection();
+    updateImage(catDirection);
+    playRound(playerDirection, catDirection);
+}
+
+// események
+window.addEventListener("load", setInitialImage);
+
 leftButton.addEventListener("click", () => {
-    const direction = generateRandomDirection();
-    updateImage(direction);
+    playGame("left");
 });
 
 upButton.addEventListener("click", () => {
-    const direction = generateRandomDirection();
-    updateImage(direction);
+    playGame("up");
 });
 
 rightButton.addEventListener("click", () => {
-    const direction = generateRandomDirection();
-    updateImage(direction);
+    playGame("right");
 });
 
 
